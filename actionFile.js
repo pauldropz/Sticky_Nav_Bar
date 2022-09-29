22504352ac2491f6c6269b1cf3aa6065b7cf739f
pauldropz <pauldropz@gmail.com>
2022-09-29 10:19:07 +0000

    js file seperated && waka time key inserted

---
+++ actionFile.js
@@ -0,0 +1,14 @@
+let navBar = document.getElementById("navBar");
+let menuBar = document.getElementById("menuBar");
+
+window.onscroll = function () {
+    if (window.pageYOffset >= menu.offsetTop) {
+        navBar.classList.add ("sticky");
+    }
+    else {
+        navBar.classList.remove("sticky");
+    };
+}
+
+// alert("Welcome  dropz");
+
